41fc1160bb75d3d73087ca0ef6d21784132b9d10
Ayon Jodder <ayonjodder177@gmail.com>
2022-06-30 18:51:52 +0600

    Custom link added

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#43B0EF",
+          "secondary": "#F9F9F9",
+          "accent": "#0F1729",
+          "neutral": "#292929",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
