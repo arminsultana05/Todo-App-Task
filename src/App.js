d86b0e4e4aed5301591e9df56b43526bdca674ef
Ayon Jodder <ayonjodder177@gmail.com>
2022-07-01 06:00:17 +0600

    Home Page done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import CompletedTask from './Components/CompletedTask/CompletedTask';
+import Home from './Components/Home/Home';
 import Navbar from './Components/Shared/Navbar';
 import ToDo from './Components/ToDo/ToDo';
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Navbar />
       <Routes>
+        <Route path='/' element={<Home/>}></Route>
         <Route path='/todo' element={<ToDo/>}></Route>
         <Route path='/completed-task' element={<CompletedTask/>}></Route>
       </Routes>
