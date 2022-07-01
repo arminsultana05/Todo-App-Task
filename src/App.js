2aa099a16872662eac7b2355abbcd3ca7c40a5e1
Ayon Jodder <ayonjodder177@gmail.com>
2022-07-01 07:18:16 +0600

    Calendar added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Components/Home/Home';
 import Navbar from './Components/Shared/Navbar';
 import ToDo from './Components/ToDo/ToDo';
 import { Toaster } from 'react-hot-toast';
+import CalendarPage from './Components/Calendar/CalendarPage';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Home/>}></Route>
         <Route path='/todo' element={<ToDo/>}></Route>
         <Route path='/completed-task' element={<CompletedTask/>}></Route>
+        <Route path='/calendar' element={<CalendarPage/>}></Route>
       </Routes>
       <Toaster />
     </div>
